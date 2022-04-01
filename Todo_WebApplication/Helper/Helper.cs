78b2164015b3c776f49a0710aab31141d7928d5e
megha dhote <megha.mathankar@gmail.com>
2022-04-01 08:29:47 +0100

    New Changes-Todo Controller

--- Todo_WebApplication/Helper/Helper.cs
+++ Todo_WebApplication/Helper/Helper.cs
@@ -11,7 +11,8 @@ namespace Todo_WebApplication.Helper
          public  HttpClient Intial()
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44379/");
+            client.BaseAddress = new Uri("https://localhost:4437/");
+           // client.BaseAddress = new Uri("https://localhost:44379/");
             return client;
         }
     }
